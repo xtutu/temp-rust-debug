68fd4f12090d2746b3db303d1f1432b10f08096e
linyongxing <xtutu0202@gmail.com>
2020-07-27 11:25:10 +0800

    add cargo.lock & revert to build release failed.

--- rust-template/src/network/network_mgr.rs
+++ rust-template/src/network/network_mgr.rs
@@ -200,122 +200,121 @@ impl NetPeer {
         return ret;
     }
     pub async fn connect(&self, mut address: String) {
-        // let id = self.id;
-        // let channel_sender_for_receiver = self.channel_sender_clone.clone();
-        // let mut tcp_writer_temp = None;
-        // {
-        //     let mut tcp_write_guard = self.tcp_writer.lock().await;
-        //     if tcp_write_guard.is_some() {
-        //         tcp_writer_temp = tcp_write_guard.take();
-        //     }
-        // }
-        //
-        // if let Some(mut xxxx) = tcp_writer_temp {
-        //     xxxx.shutdown().await;
-        // }
-        //
-        // log::debug!("do connect: {}", address);
-        // let address_parse_result = address.parse::<SocketAddr>();
-        // let mut addr;
-        // if let Ok(temp_addr) = address_parse_result {
-        //     addr = temp_addr.to_string()
-        // }else{
-        //     // 尝试解析dns
-        //     let list :Vec<_>= address.split(":").collect();
-        //     let dns_rst = dns_lookup::lookup_host(list[0]).unwrap();
-        //     addr = format!("{}:{}", dns_rst[0], list[1]);
-        //     addr = "127.0.0.1".to_string();
-        //     // log::info!("host => addr: {}", &addr);
-        // }
-        //
-        // let stream = TcpStream::connect(addr).await;
-        // // let stream = futures::executor::block_on(TcpStream::connect(addr));
-        // if let Err(ref e) = stream {
-        //     log::error!("{}", e);
-        //     self.channel_sender_clone.send(channel_msg::Msg::OnTcpConnectError(id));
-        //     // return Ok(());
-        //     return;
-        // }
-        // log::info!("NetPeer {}, connect success {}",id, address);
-        // let stream = stream.unwrap();
-        // self.channel_sender_clone.send(channel_msg::Msg::OnTcpConnected(id));
-        //
-        // // stream.shutdown(std::net::Shutdown::Both);
-        // let rr: Option<ReadHalf<TcpStream>>;
-        // {
-        //     // 这个是有效的
-        //     // stream.shutdown(std::net::Shutdown::Both);
-        //     // 这个也是有效的
-        //     // stream.shutdown(std::net::Shutdown::Write);
-        //
-        //     // 这样貌似也是可以的？不过这里可能是因为 stream 本身被 drop 了
-        //     // let mut stream = stream;
-        //     // let (mut tr , mut tw ) = stream.split();
-        //     // tw.shutdown();
-        //
-        //     let (tr, mut tw) = tokio::io::split(stream);
-        //     // tw.poll_shutdown()
-        //     // tw shutdown 没有用
-        //     // tw.shutdown();
-        //     let mut tcp_writer_guard = self.tcp_writer.lock().await;
-        //     *tcp_writer_guard = Some(tw);
-        //
-        //     rr = Some(tr);
-        // }
-        // tokio::spawn(async move {
-        //     let mut head_buf = [0; 10];
-        //     let mut r = rr.unwrap();
-        //     loop {
-        //         let receive_result = r.read_exact(&mut head_buf).await;
-        //         if let Err(e) = receive_result {
-        //             log::debug!("NetPeer OnTcpClosed {}", id);
-        //             channel_sender_for_receiver.send(channel_msg::Msg::OnTcpClosed(id, e.to_string()));
-        //             break;
-        //         }
-        //         let received_len = receive_result.unwrap();
-        //
-        //         // 实际上应该是不需要检查的
-        //         if received_len != 10 {
-        //             log::debug!("unused check {}", received_len)
-        //         }
-        //         let len = LittleEndian::read_u32(&head_buf[0..4]) as usize;
-        //         let msg_id = LittleEndian::read_u16(&head_buf[4..6]);
-        //         let guess_size = LittleEndian::read_i32(&head_buf[6..10]);
-        //         let guess_size = guess_size as usize;
-        //
-        //         log::trace!("len {} msg_id {} guess_size {}", len, msg_id, guess_size);
-        //
-        //         let data_size = len - 6;
-        //         // let mut data= Vec::<u8>::with_capacity(data_size).resize();
-        //         let mut data = vec![0; data_size];
-        //         // log::debug!("data size {} {}", data.as_mut_slice().len(), data_size);
-        //         if len > 0 {
-        //             let received_result = r.read_exact(data.as_mut_slice()).await;
-        //             let received = try_handle_error!(received_result);
-        //             if received != data_size {
-        //                 log::error!("received size error {} {}", received, data_size);
-        //                 break;
-        //             }
-        //
-        //             let final_data: BString;
-        //             if guess_size == 0 {
-        //                 final_data = BString::from(data);
-        //             } else {
-        //                 let result = lz4::block::decompress(data.as_slice(), Some(guess_size as i32));
-        //                 if let Err(e) = result {
-        //                     log::error!("decompress error {:}", e);
-        //                     break;
-        //                 }
-        //                 final_data = BString::from(result.unwrap());
-        //             }
-        //
-        //
-        //             try_handle_error!(channel_sender_for_receiver.send(channel_msg::Msg::OnTcpReceived(id, final_data)));
-        //         } else {
-        //             log::error!("error in received len = 0")
-        //         }
-        //     }
-        // });
+        let id = self.id;
+        let channel_sender_for_receiver = self.channel_sender_clone.clone();
+        let mut tcp_writer_temp = None;
+        {
+            let mut tcp_write_guard = self.tcp_writer.lock().await;
+            if tcp_write_guard.is_some() {
+                tcp_writer_temp = tcp_write_guard.take();
+            }
+        }
+
+        if let Some(mut xxxx) = tcp_writer_temp {
+            xxxx.shutdown().await;
+        }
+
+        log::debug!("do connect: {}", address);
+        let address_parse_result = address.parse::<SocketAddr>();
+        let mut addr;
+        if let Ok(temp_addr) = address_parse_result {
+            addr = temp_addr.to_string()
+        }else{
+            // 尝试解析dns
+            let list :Vec<_>= address.split(":").collect();
+            let dns_rst = dns_lookup::lookup_host(list[0]).unwrap();
+            addr = format!("{}:{}", dns_rst[0], list[1]);
+            // log::info!("host => addr: {}", &addr);
+        }
+
+        let stream = TcpStream::connect(addr).await;
+        // let stream = futures::executor::block_on(TcpStream::connect(addr));
+        if let Err(ref e) = stream {
+            log::error!("{}", e);
+            self.channel_sender_clone.send(channel_msg::Msg::OnTcpConnectError(id));
+            // return Ok(());
+            return;
+        }
+        log::info!("NetPeer {}, connect success {}",id, address);
+        let stream = stream.unwrap();
+        self.channel_sender_clone.send(channel_msg::Msg::OnTcpConnected(id));
+
+        // stream.shutdown(std::net::Shutdown::Both);
+        let rr: Option<ReadHalf<TcpStream>>;
+        {
+            // 这个是有效的
+            // stream.shutdown(std::net::Shutdown::Both);
+            // 这个也是有效的
+            // stream.shutdown(std::net::Shutdown::Write);
+
+            // 这样貌似也是可以的？不过这里可能是因为 stream 本身被 drop 了
+            // let mut stream = stream;
+            // let (mut tr , mut tw ) = stream.split();
+            // tw.shutdown();
+
+            let (tr, mut tw) = tokio::io::split(stream);
+            // tw.poll_shutdown()
+            // tw shutdown 没有用
+            // tw.shutdown();
+            let mut tcp_writer_guard = self.tcp_writer.lock().await;
+            *tcp_writer_guard = Some(tw);
+
+            rr = Some(tr);
+        }
+        tokio::spawn(async move {
+            let mut head_buf = [0; 10];
+            let mut r = rr.unwrap();
+            loop {
+                let receive_result = r.read_exact(&mut head_buf).await;
+                if let Err(e) = receive_result {
+                    log::debug!("NetPeer OnTcpClosed {}", id);
+                    channel_sender_for_receiver.send(channel_msg::Msg::OnTcpClosed(id, e.to_string()));
+                    break;
+                }
+                let received_len = receive_result.unwrap();
+
+                // 实际上应该是不需要检查的
+                if received_len != 10 {
+                    log::debug!("unused check {}", received_len)
+                }
+                let len = LittleEndian::read_u32(&head_buf[0..4]) as usize;
+                let msg_id = LittleEndian::read_u16(&head_buf[4..6]);
+                let guess_size = LittleEndian::read_i32(&head_buf[6..10]);
+                let guess_size = guess_size as usize;
+
+                log::trace!("len {} msg_id {} guess_size {}", len, msg_id, guess_size);
+
+                let data_size = len - 6;
+                // let mut data= Vec::<u8>::with_capacity(data_size).resize();
+                let mut data = vec![0; data_size];
+                // log::debug!("data size {} {}", data.as_mut_slice().len(), data_size);
+                if len > 0 {
+                    let received_result = r.read_exact(data.as_mut_slice()).await;
+                    let received = try_handle_error!(received_result);
+                    if received != data_size {
+                        log::error!("received size error {} {}", received, data_size);
+                        break;
+                    }
+
+                    let final_data: BString;
+                    if guess_size == 0 {
+                        final_data = BString::from(data);
+                    } else {
+                        let result = lz4::block::decompress(data.as_slice(), Some(guess_size as i32));
+                        if let Err(e) = result {
+                            log::error!("decompress error {:}", e);
+                            break;
+                        }
+                        final_data = BString::from(result.unwrap());
+                    }
+
+
+                    try_handle_error!(channel_sender_for_receiver.send(channel_msg::Msg::OnTcpReceived(id, final_data)));
+                } else {
+                    log::error!("error in received len = 0")
+                }
+            }
+        });
     }
 
     pub async fn close(&self) {
